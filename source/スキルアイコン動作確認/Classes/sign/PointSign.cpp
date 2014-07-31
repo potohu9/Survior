9a91efe750a53b7e9bc7aa390999ea302c97bf4f
twfurifuri <eu1i4sil@gmail.com>
2014-07-31 21:20:57 +0900

    スキルアイコンの配置が正しく行われているか確認お願いします
    各フォルダをコピーして実行してください。
    Android.mkはjniフォルダ内に上書きコピーしてください

---
+++ source/スキルアイコン動作確認/Classes/sign/PointSign.cpp
@@ -0,0 +1,10 @@
+/*
+ * PointSign.cpp
+ *
+ *  Created on: 2014/07/31
+ *      Author: scc313
+ */
+
+
+
+
---
+++ source/スキルアイコン動作確認/Classes/sign/PointSign.h
@@ -0,0 +1,15 @@
+/*
+ * PointSign.h
+ *
+ *  Created on: 2014/07/31
+ *      Author: scc313
+ */
+
+#ifndef POINTSIGN_H_
+#define POINTSIGN_H_
+
+
+
+
+
+#endif /* POINTSIGN_H_ */
