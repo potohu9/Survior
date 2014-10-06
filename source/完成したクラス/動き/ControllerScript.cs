8cb0c419925ad61d2e93f3dddb69f71e49d97734
potohu9 <meruhularia@gmail.com>
2014-10-06 13:27:13 +0900

    動き制御

---
+++ source/完成したクラス/動き/ControllerScript.cs
@@ -0,0 +1,81 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ControllerScript : MonoBehaviour {
+
+	//private EnemyMoveScript enemyMoveScript;
+	private Animator animator;
+	private GameObject oya;
+
+	private bool animeFlag=true;
+	private int animeMode=0;
+	private int count=0;
+
+	//EnemyMoveFactory enemyMoveFactory;
+	AbstractEnemyAIController straight;
+	AbstractEnemyAIController wait;
+
+
+	// Use this for initialization
+	void Start () {
+		animator = GetComponent<Animator> ();
+		oya = gameObject.transform.parent.gameObject;
+
+		//enemyMoveScript = new EnemyMoveScript (oya,animator);
+		animator.SetInteger ("isAnime", 0);
+
+		//enemyMoveFactory = new EnemyMoveFactory(oya,animator);
+		straight = new Straight1 (oya,animator);
+		wait = new Wait1 (oya, animator);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(animeFlag){
+			if (Input.GetKey("up")) {
+				animeFlag=false;
+				animeMode=1;
+			} else if(Input.GetKey("down")){
+				animeFlag=false;
+				animeMode=2;
+			}
+			if (Input.GetKey("right")) {
+				//oya.transform.Rotate (0, 1, 0);
+				animeFlag=false;
+				animeMode=3;
+			}else if (Input.GetKey ("left")) {
+				oya.transform.Rotate (0, -1, 0);
+				animeFlag=false;
+				animeMode=4;
+			}
+		} else {
+			count++;
+		}
+
+		switch(animeMode){
+		case 0:
+			//enemyMoveScript.wait();
+			wait.move (count);
+			break;
+		case 1:
+			//enemyMoveScript.straight();
+			straight.move (count);
+			break;
+		case 2:
+			//enemyMoveScript.smallSlalom(count);
+			break;
+		case 3:
+			//enemyMoveScript.RlargeSlalom(count);
+			break;
+		case 4:
+			//enemyMoveScript.LlargeSlalom(count);
+			break;
+		}
+	}
+
+	void setAnimeFlag(){
+		animeFlag=true;
+		animeMode=0;
+		count = 0;
+	}
+}
---
+++ source/完成したクラス/動き/moveScript.cs
@@ -0,0 +1,72 @@
+﻿using UnityEngine;
+using System.Collections;
+// 待機1
+public class Wait1 : AbstractEnemyAIController {
+	GameObject oyaobj;
+	Animator animator;
+	public Wait1(GameObject obj,Animator ani){
+		oyaobj = obj;
+		animator = ani;
+	}
+	public override void move(int count){
+		animator.SetInteger ("isAnime", 0);
+	}
+}
+// 直進1
+public class Straight1 : AbstractEnemyAIController {
+	GameObject oyaobj;
+	Animator animator;
+	public Straight1(GameObject obj,Animator ani){
+		oyaobj = obj;
+		animator = ani;
+	}
+	public override void move(int count){
+		animator.SetInteger ("isAnime", 1);
+		oyaobj.transform.Translate(Vector3.forward * 0.1f);
+	}
+}
+// 細かいジグザグ
+public class SmallSlalom1 : AbstractEnemyAIController {
+	GameObject oyaobj;
+	Animator animator;
+	public SmallSlalom1(GameObject obj,Animator ani){
+		oyaobj = obj;
+		animator = ani;
+	}
+	public override void move(int count){
+		animator.SetInteger ("isAnime", 2);
+		oyaobj.transform.Translate(Vector3.forward * 0.1f);
+		float sinnum = 0.1f * Mathf.Cos (count * 0.1f);
+		oyaobj.transform.Translate(Vector3.right * sinnum);
+	}
+}
+//大きなジグザグ右
+public class RlargeSlalom1 : AbstractEnemyAIController {
+	GameObject oyaobj;
+	Animator animator;
+	public RlargeSlalom1(GameObject obj,Animator ani){
+		oyaobj = obj;
+		animator = ani;
+	}
+	public override void move(int count){
+		animator.SetInteger ("isAnime", 3);
+		oyaobj.transform.Translate(Vector3.forward * 0.1f);
+		float sinnum = 0.7f * Mathf.Sin (count/13 * 0.75f);
+		oyaobj.transform.Translate(Vector3.right * sinnum);
+	}
+}
+// 大きなジグザグ左
+public class LlargeSlalom1 : AbstractEnemyAIController {
+	GameObject oyaobj;
+	Animator animator;
+	public LlargeSlalom1(GameObject obj,Animator ani){
+		oyaobj = obj;
+		animator = ani;
+	}
+	public override void move(int count){
+		animator.SetInteger ("isAnime", 3);
+		oyaobj.transform.Translate(Vector3.forward * 0.1f);
+		float sinnum = 0.7f * Mathf.Sin (count/13 * 0.75f);
+		oyaobj.transform.Translate(Vector3.right * -sinnum);
+	}
+}
